6cab4cf79b4275e010e7dfc767c4e15a677ec7ef
khellendros <krhproductions@gmail.com>
2019-10-06 16:24:12 -0400

    completed initial script

---
+++ dotsetup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#create symlinks for dot files
+
+if [ ! -d ~/.irssi/ ]; then
+	mkdir ~/.irssi
+fi
+
+rm ~/.irssi/config
+ln -s ~/dotfiles/.irssi/config ~/.irssi/config
+
+rm ~/.bashrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+
+rm ~/.vimrc
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+rm ~/.tmux.conf
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+rm ~/.gdbinit
+ln -s ~/dotfiles/.gdbinit ~/.gdbinit
+
